c1a6eacb72acb05d36b398f95416220c819b6789
pig-08 <pswhoonsmt@gmail.com>
2024-05-29 22:01:56 +0900

    전투씬 제작중1

---
+++ Smile/Assets/Script/BattleStars1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleStars1 : MonoBehaviour
+{
+    [SerializeField]private bool _check;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            _check = true;
+        }
+    }
+}

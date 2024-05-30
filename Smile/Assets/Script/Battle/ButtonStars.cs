374ac43819ce1070821d72af2f2d2ceded5b4bc4
pig-08 <pswhoonsmt@gmail.com>
2024-05-30 19:10:32 +0900

    전투씬제작3

--- Smile/Assets/Script/Battle/ButtonStars.cs
+++ Smile/Assets/Script/Battle/ButtonStars.cs
@@ -5,28 +5,16 @@ using UnityEngine.UI;
 
 public class ButtonStars : MonoBehaviour
 {
-    private bool[] _check = new bool[5];
-    private Button[] _stars = new Button[5];
+    [SerializeField]private bool[] _check = new bool[5];
+    private Button _stars;
     private void Awake()
     {
-        _stars[0] = GameObject.Find("Stars").GetComponent<Button>();
-        _stars[1] = GameObject.Find("Stars(1)").GetComponent<Button>();
-        _stars[2] = GameObject.Find("Stars(2)").GetComponent<Button>();
-        _stars[3] = GameObject.Find("Stars(3)").GetComponent<Button>();
-        _stars[4] = GameObject.Find("Stars(4)").GetComponent<Button>();
-        _stars[0].onClick.AddListener(() => ButtonCkeck(0));
-        _stars[1].onClick.AddListener(() => ButtonCkeck(1));
-        _stars[2].onClick.AddListener(() => ButtonCkeck(2));
-        _stars[3].onClick.AddListener(() => ButtonCkeck(3));
-        _stars[4].onClick.AddListener(() => ButtonCkeck(4));
+        _stars =GetComponent<Button>();
+        _stars.onClick.AddListener(() => ButtonCkeck((gameObject.name[5])));
     }
     public void ButtonCkeck(int i)
     {
+        i -= 48;
         _check[i] = true;
-    }
-
-    private void LineCkeck()
-    {
-
     }
 }
